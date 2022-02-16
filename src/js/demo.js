215413cf5998377690046f3c849f0ea1f97a4c69
cxh <2604856589@qq.com>
2022-02-16 23:52:08 +0800

    feat: 模板初始化

---
+++ src/js/demo.js
@@ -0,0 +1,12 @@
+const get1 = () => {
+  console.log('get1');
+};
+
+const get2 = () => {
+  console.log('get2');
+};
+
+export {
+  get1,
+  get2,
+};
---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+import '../css/index.css';
+import '../css/index.less';
+import { createApp } from 'vue';
+import $ from 'jquery';
+import { get1 } from './demo';
+
+console.log('createApp', $);
+console.log('createApp', createApp);
+
+get1();
+
+const a = () => {
+  console.log('我是A');
+};
+
+a();
+
+const b = new Promise((res) => {
+  res(123);
+}).then((res) => {
+  console.log('res', res);
+});
+
+console.log('b', b);
